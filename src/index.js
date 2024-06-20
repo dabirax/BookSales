80d6964997cefae9442795541b346e50bd12f578
dabirax <oliverpraise1@gmail.com>
2024-06-20 10:09:29 +0100

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,192 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import picone from "./images/one.jpg";
+// import pictwo from "./images/two.jpg";
+// import picthree from "./images/three.jpg";
+import "./output.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const Greeting = () => {
+//   return (
+//     <>
+//       <div>
+//         <h3>My First Component</h3>
+//         <ul>
+//           <li>
+//             <a href="#">Hello world</a>
+//           </li>
+//         </ul>
+//       </div>
+//       <h2>Gehazi</h2>
+//     </>
+//   );
+// };
+// const Greeting = () => {
+//   return React.createElement('div', {}, React.createElement('h2',{},'hello world'));
+// };
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Nesting
+
+// const Person = () => <h2>John the Baptist</h2>;
+// const Message = () => {
+//   return <h4>Seven horns ...</h4>;
+// };
+
+// function Greeting() {
+//   return (
+//     <div>
+//       <Person />
+//       <Message />
+//     </div>
+//   );
+// }
+
+const firstBook = {
+  author: "Jordan Moore",
+  title: "Witnessing in the HS",
+  img: "./images/one.jpg",
+};
+const secondBook = {
+  author: "Praise Nelson",
+  title: "Atomic Habits",
+  img: "./images/two.jpg",
+};
+const thirdBook = {
+  author: "George Myles",
+  title: "Reassessing Your Chess Moves",
+  img: "./images/three.jpg",
+};
+
+// const BookList = () => {
+//   return (
+//     <section className="w-[90vw] max-w-6xl m-20 grid gap-8 md:grid-cols-3">
+//       <Book
+//         author={firstBook.author}
+//         title={firstBook.title}
+//         img={firstBook.img}
+//       />
+//       <Book
+//         author={secondBook.author}
+//         title={secondBook.title}
+//         img={secondBook.img}
+//       />
+//       <Book
+//         author={thirdBook.author}
+//         title={thirdBook.title}
+//         img={thirdBook.img}
+//       />
+//       <Book
+//         author={secondBook.author}
+//         title={secondBook.title}
+//         img={secondBook.img}
+//       />
+//       <Book
+//         author={thirdBook.author}
+//         title={thirdBook.title}
+//         img={thirdBook.img}
+//       />
+//       <Book
+//         author={firstBook.author}
+//         title={firstBook.title}
+//         img={firstBook.img}
+//       />
+//     </section>
+//   );
+// };
+// const Book = (props) => {
+//   return (
+//     <article className="bg-white rounded-2xl p-8 text-center">
+//       <Image img={props.img} title={props.title} />
+//       <Title title={props.title} />
+//       <Author author={props.author} />
+//     </article>
+//   );
+// };
+// const Image = (props) => {
+//   return (
+//     <img src={props.img} alt={props.title} className="w-96 object-cover" />
+//   );
+// };
+// const Title = (props) => {
+//   return <h2 className="mt-4 text-base">{props.title}</h2>;
+// };
+// const Author = (props) => {
+//   return (
+//     <h4 className="mt-2 text-[#617d98] text-xs tracking-[2px]">
+//       {props.author}
+//     </h4>
+//   );
+// };
+
+
+// ShortCut for props
+const BookList = () => {
+  return (
+    <section className="w-[90vw] max-w-6xl m-20 grid gap-8 md:grid-cols-3">
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
+      <Book
+        author={thirdBook.author}
+        title={thirdBook.title}
+        img={thirdBook.img}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
+      <Book
+        author={thirdBook.author}
+        title={thirdBook.title}
+        img={thirdBook.img}
+      />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
+    </section>
+  );
+};
+const Book = (props) => {
+  console.log(props)
+   const {author, title, img} = props;
+  return (
+    <article className="bg-white rounded-2xl p-8 text-center">
+      <Image img={img} title={title} />
+      <Title title={title} />
+      <Author author={author} />
+    </article>
+  );
+};
+const Image = (props) => {
+   const {title, img } = props;
+  return (
+    <img src={img} alt={title} className="w-96 object-cover" />
+  );
+};
+const Title = (props) => {
+   const {title } = props;
+  return <h2 className="mt-4 text-base">{title}</h2>;
+};
+const Author = (props) => {
+   const {author} = props;
+  return (
+    <h4 className="mt-2 text-[#617d98] text-xs tracking-[2px]">
+      {author}
+    </h4>
+  );
+};
+
+export default BookList;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<BookList />);
